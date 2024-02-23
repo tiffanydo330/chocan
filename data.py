2b6b3f33f9f4efdd254a2b3c330ec591dcbfabe4
jaden <price.jaden99@gmail.com>
2024-02-22 19:17:31 -0800

    finally working holy fuck, no dict tho

--- data.py
+++ data.py
@@ -1,13 +1,5 @@
-#filename:   data.py
-#summary:    This file is responsible for setting up the core class heirarchy that will
-#            be used for most(all?) information that is necessary to be stored and
-#            managed by the ChocAn system.
-
-# the person class will be responsible for the data that both members & providers share
-
-#import numpy 
-from datetime import datetime
 from marshmallow import Schema, fields, post_load
+from datetime import datetime
 
 class Person:
     def __init__(self, name, id_num, street, city, state, zip_code):
@@ -18,52 +10,22 @@ class Person:
         self.state = state
         self.zip_code = zip_code
 
-class PersonSchema(Schema):
-    name = fields.Str()
-    id_num = fields.Str()
-    street = fields.Str()
-    city = fields.Str()
-    state = fields.Str()
-    zip_code = fields.Str()
-
 class Member(Person):
     def __init__(self, name, id_num, street, city, state, zip_code, services):
         super().__init__(name, id_num, street, city, state, zip_code)
-        self.services = services 
-
-class MemberSchema(PersonSchema):
-    services = fields.Nested('MemberServiceSchema', many=True)
-            
-    @post_load
-    def make_member(self, data, **kwargs):
-        return Member(**data)
+        self.services = services
 
 class Provider(Person):
-    def __init__(self, name, id_num, street, city, state, zip_code, 
-            services, num_consultations, total_wk_fee):
+    def __init__(self, name, id_num, street, city, state, zip_code,
+                 services, num_consultations, total_wk_fee):
         super().__init__(name, id_num, street, city, state, zip_code)
         self.services = services
         self.num_consultations = num_consultations
         self.total_wk_fee = total_wk_fee
-class ProviderSchema(PersonSchema):
-    services = fields.Nested('ProviderServiceSchema', many=True)
-    num_consultations = fields.Integer()
-    total_wk_fee = fields.Float()
-
-    @post_load
-    def make_provider(self, data, **kwargs):
-        return Provider(**data)
 
 class Service:
     def __init__(self, date):
         self.date = date
-
-class ServiceSchema(Schema):
-    date = fields.Date(format='%m-%d-%Y')
-
-    @post_load
-    def make_service(self, data, **kwargs):
-        return Service(**data)
 
 class MemberService(Service):
     def __init__(self, date, provider_name, service_name):
@@ -71,31 +33,14 @@ class MemberService(Service):
         self.provider_name = provider_name
         self.service_name = service_name
 
-class MemberServiceSchema(ServiceSchema):
-    provider_name = fields.Str()
-    service_name = fields.Str()
-
-    @post_load
-    def make_member_service(self, data, **kwargs):
-        return MemberService(**data)
-
 class ProviderService(Service):
-    def __init__(self, date, date_and_time, member_name, member_number, service_code, 
-                 fee):
+    def __init__(self, date, date_and_time, member_name, member_number, service_code,
+                 fee, num_consultations, total_fee):
         super().__init__(date)
         self.date_and_time = date_and_time
         self.member_name = member_name
         self.member_number = member_number
         self.service_code = service_code
         self.fee = fee
-
-class ProviderServiceSchema(ServiceSchema):
-    date_and_time = fields.DateTime(format='%m-%d-%Y %H:%M:%S')
-    member_name = fields.Str()
-    member_number = fields.Str()
-    service_code = fields.Str()
-    fee = fields.Float()
-
-    @post_load
-    def make_provider_service(self, data, **kwargs):
-        return ProviderService(**data)
+        self.num_consultations = num_consultations
+        self.total_fee = total_fee
--- main.py
+++ main.py
@@ -1,67 +1,44 @@
-#main.py
-
-#from marshmallow import Schema, fields, post_load
 import json
-from data import *
+from data import Member, Provider
+from schema import MemberSchema, ProviderSchema
 
-def write_json(data, filename):
-    with open(filename, 'w') as f:
-        json.dump(data, f, indent=4)
-
-def read_json(filename):
-    with open(filename, 'r') as f:
-        data = json.load(f)
+def load_data_from_json(filename):
+    with open(filename, 'r') as file:
+        data = json.load(file)
     return data
 
+def write_data_to_json(data, filename):
+    with open(filename, 'w') as file:
+        json.dump(data, file, indent=4)
+
 def main():
-    date_string1 = '01-01-2001'
-    date_obj = datetime.strptime(date_string1, '%m-%d-%Y')
-    datetime_string1 = '01-02-2001 07:56:30'
-    datetime_obj = datetime.strptime(datetime_string1, '%m-%d-%Y %H:%M:%S')
+    # Load data from JSON file
+    member_data = load_data_from_json('members.json')
+    provider_data = load_data_from_json('providers.json')
 
-    member1services = [MemberService(date_obj, "provider1", "service")]
-    member2services = [MemberService(date_obj, "provider2", "service")]
+    # Deserialize data
+    member_schema = MemberSchema(many=True)
+    provider_schema = ProviderSchema(many=True)
+    members = member_schema.load(member_data)
+    providers = provider_schema.load(provider_data)
     
-    provider1services = [ProviderService(date_obj, datetime_obj, 
-                                        "jon", "001", "7", 5000)]
-    provider2services = [ProviderService(date_obj, datetime_obj, 
-                                        "jane", "002", "7", 5000)]
-    members = [
-        Member("Member1", "001", "member 1 street", "member1 town", "OR", "89019",
-                member1services),
-        Member("Jane Smith", "002", "456 Oak St", "Othertown", "NY", "67890",
-                member2services)
-            ]
+    # modifies data, should be fine with user input
+    members[0].name = "New Name"
 
-    providers = [
-        Provider("Dr. Smith", "101", "789 Elm St", "Anycity", "TX", "54321",
-                provider1services, 10, 1000), 
-        Provider("Dr. Johnson", "102", "987 Pine St", "Othercity", "FL", "13579",
-                provider2services, 8, 800)
-            ]
-    member_schema = MemberSchema()
-    provider_schema = ProviderSchema()
-    
-    member_data = [member_schema.dump(member) for member in members]
-    provider_data = [provider_schema.dump(provider) for provider in providers]
+    print("Modified Members:")
+    for member in members:
+        print("Name:", member.name)
+        print("ID:", member.id_num)
+        print("Services:")
+        for service in member.services:
+            print("- Date:", service.date)
+            print("- Provider Name:", service.provider_name)
+            print("- Service Name:", service.service_name)
+        print()
 
-    write_json(member_data, 'members.json')
-    write_json(provider_data, 'providers.json')
-    
-    loaded_members_data = read_json('members.json')
-    loaded_providers_data = read_json('providers.json')
-
-    loaded_members = [member_schema.load(member) for member in loaded_members_data]
-    loaded_providers = [provider_schema.load(provider) 
-                        for provider in loaded_providers_data]
-    
-    print("Loaded members:")
-    for member in loaded_members:
-        print(member.__dict__)
-
-    print("Loaded providers:")
-    for provider in loaded_providers:
-        print(provider.__dict__)
+    # write data back to JSON files
+    write_data_to_json(member_schema.dump(members), 'members_modified.json')
+    write_data_to_json(provider_schema.dump(providers), 'providers_modified.json')
 
 if __name__ == "__main__":
     main()

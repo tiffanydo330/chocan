fc56be937b9f269e8705ec4d9f31da80b610b148
Carl-Swin <147886276+Carl-Swin@users.noreply.github.com>
2024-03-15 22:55:43 -0700

    Update test_Carl_Swin.py
    Organized file creation testing to go to a directory rather than barfing it into your C: drive

--- Testing/test_Carl_Swin.py
+++ Testing/test_Carl_Swin.py
@@ -2,10 +2,10 @@ import json  # Importing the json module to work with JSON data
 from data import Member, Provider  # Importing custom data models
 from schema import MemberSchema, ProviderSchema  # Importing schemas for data serialization
 from typing import Dict, Any  # Importing type hints for data types
-from data_management import DataManager
 from marshmallow import Schema, fields, post_load
 from Carl_Swin import sum_rep_main, EFT_file, print_format, make_dir
 from datetime import datetime
+from data_management import DataManager
 import os
 import pathlib
 import pytest
@@ -22,20 +22,27 @@ def test_sum_rep_main():
     assert fd.read() == "Dr. Smith\a100001\a250.0\nDr. Johnson\a100002\a800.0\nTotal: $1050.0"
     fd.close()
 
+def test_make_dir():
+    assert make_dir("<<>>") == 1
+
 def test_EFT_file():
+    DestPath = pathlib.Path(str(os.getcwd()) + "/Testing/test-Carl-Swin-Dir")
+    assert make_dir( "Testing/test-Carl-Swin-Dir" ) == 0
+
     assert EFT_file("", None, "", None) == 3 
-
-    assert EFT_file("", None, 123456789, None) == 2 
-    assert EFT_file("", None, "Pytest_file.txt", None) == 2 
-
+    
+    assert EFT_file(DestPath, None, 123456789, None) == 2 
+    assert EFT_file(DestPath, None, "Pytest-file.txt", None) == 2 
+    
     for id_num, Prov in providers_dict.items():
-        assert EFT_file("", Prov, "Pytest_file.txt", None) == 1 
+        assert EFT_file(DestPath, Prov, "Pytest-file.txt", None) == 1 
+
+    #os.remove( pathlib.Path(str(DestPath) + "/123456789") )
+    #os.remove( pathlib.Path(str(DestPath) + "/Pytest-file.txt") )
+
 
 def test_print_format():
     assert print_format("", None) == 1
 
     for id_num, Prov in providers_dict.items():
         assert print_format(id_num, Prov) == 0
-
-def test_make_dir():
-    assert make_dir("<<>>") == 1
